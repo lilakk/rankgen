64f234c4c11be90937bef09fa0aee55470b7e969
Yapei Chang <yapeichang@wpa092.wpanet.cs.local>
2022-09-08 12:56:10 -0400

    implementing backprop on input embedding

--- rankgen/rankgen_textgen.py
+++ rankgen/rankgen_textgen.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 
 rankgen_encoder = RankGenEncoder(model_path=args.rankgen_encoder, cache_dir=args.cache_dir)
 rankgen_generator = RankGenGenerator(rankgen_encoder=rankgen_encoder, language_model="gpt2-medium", cache_dir=args.cache_dir)
-
+model = RankGenEncoder(model_path="kalpeshk2011/rankgen-t5-xl-all", cache_dir=None)
 
 def loss_fn(prefix_vector, suffix_vector):
     similarity = torch.matmul(prefix_vector, suffix_vector.t()).squeeze(dim=0)
@@ -23,13 +23,13 @@ def loss_fn(prefix_vector, suffix_vector):
 def textgen(prefix, suffix, epochs):
     prefix_vector = rankgen_encoder.encode(prefix, vectors_type="prefix")["embeddings"]
     suffix_vector = rankgen_encoder.encode(suffix, vectors_type="suffix")["embeddings"]
-    optimizer = torch.optim.SGD([suffix_vector], lr=0.001, momentum=0.9)
+    optimizer = torch.optim.SGD(rankgen_encoder.model.t5_encoder.shared, lr=0.001, momentum=0.9)
     for i in range(epochs):
         print(f"Epoch {i}")
         optimizer.zero_grad()
-        suffix_vector.requires_grad = True
-        for param in rankgen_encoder.model.parameters():
-            param.requires_grad = False
+        # suffix_vector.requires_grad = True
+        # for param in rankgen_encoder.model.parameters():
+        #     param.requires_grad = False
         loss = loss_fn(prefix_vector, suffix_vector)
         print(f"loss: {loss}")
         print(f"suffix vector gradient: {suffix_vector.grad}")
@@ -38,7 +38,7 @@ def textgen(prefix, suffix, epochs):
     return loss
 
 
-pre = "pre For two years, schools and researchers have wrestled with pandemic-era learning setbacks resulting mostly from a lack of in-person classes."
-suf = "suf You"
+pre = "For two years, schools and researchers have wrestled with pandemic-era learning setbacks resulting mostly from a lack of in-person classes."
+suf = "You"
 
 textgen(pre, suf, 10)
